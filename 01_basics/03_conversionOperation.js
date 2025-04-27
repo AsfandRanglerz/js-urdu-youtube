2d200c6f9f340ec6473af657b3242e059b4b70ff
AsfandRanglerz <72138600+AsfandRanglerz@users.noreply.github.com>
2025-04-27 13:50:19 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,44 @@ console.log('isLoggedIn', booleanIsLoggedIn)
 // 1 => true; 0 => false
 // '' => false
 // 'alpha' => true
+
+/* Operations */
+let val = 3
+let negVal = -val
+console.log(negVal);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%3)
+// console.log(2**2)
+// console.log(2**3)
+
+let firstName = 'Max'
+let lastName = 'Tyson'
+console.log(firstName + ' ' + lastName)
+
+// console.log('1' + '2')
+// console.log('1' + 2)
+// console.log(1 + '2')
+// console.log(1 + '2' + 2)
+// console.log(1 + 2 + '2')
+// console.log(1 + (2 + '2'))
+
+// console.log(true)
+// console.log(+true)
+// console.log(+'')
+
+// let num1, num2, num3
+
+// num1 = 5
+// num2 = num1
+// num3 = num2
+// console.log([num1, num2, num3])
+
+let gameCounter = 1;
+console.log(gameCounter);
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
