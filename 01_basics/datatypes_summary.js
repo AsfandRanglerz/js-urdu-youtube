5c7e015e489b4519e9f029c4057364353c8d047e
AsfandRanglerz <72138600+AsfandRanglerz@users.noreply.github.com>
2025-04-27 17:24:47 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+// Primitive
+// 7 types : string, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference/Non-Primitive
+// Arrays, Objects, Functions
+
+// const score:number = 100 // typescript
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log('Testing:', id == anotherId);
+
+const bigNumber = 34554234234324324255435n
+
+const heroes = ['superman', 'batman', 'spiderman']
+
+let myObj = {
+    name: 'Max',
+    age: 20
+}
+
+const myFunction = function() {
+    console.log('Hello World')
+}
+myFunction()
+
+console.log(typeof (bigNumber));
+console.log(typeof (myFunction));
