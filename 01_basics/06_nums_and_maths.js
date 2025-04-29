19ffc247ad795f293e16b284001f0072a568c254
AsfandRanglerz <72138600+AsfandRanglerz@users.noreply.github.com>
2025-04-29 18:51:41 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+/***** Nums *****/
+// const score = 400
+// console.log(score)
+// console.log(score.toFixed(2));
+
+// const balance = new Number(400)
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherBalance = new Number(25.45675)
+// console.log(otherBalance.toFixed(2));
+// console.log(otherBalance.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'))
+
+/***** Maths *****/
+// console.log(Math.abs(4))
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.7))
+// console.log(Math.ceil(4.7))
+// console.log(Math.floor(4.7))
+
+// console.log(Math.min(3, 5, 7, 9));
+// console.log(Math.max(3, 5, 7, 9));
+
+// console.log(Math.random());
+console.log(Math.random()*10);
